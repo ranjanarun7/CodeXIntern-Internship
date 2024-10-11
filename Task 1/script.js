e35b3bccc59f1e75f0e7d92f71aacce31a627daa
Arun Kumar <142811539+ranjanarun7@users.noreply.github.com>
2024-10-11 15:52:10 +0530

    Add files via upload

---
+++ Task 1/script.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector(".btn");
+const container = document.querySelector(".container");
+
+btn.addEventListener("click", (e)=>{
+  e.preventDefault()
+
+  if(btn.innerText === "Light"){
+    container.style.backgroundColor = "Black"
+    btn.innerText = "Dark"
+  }else{
+    container.style.backgroundColor = "White"
+    btn.innerText = "Light"
+  }
+})
